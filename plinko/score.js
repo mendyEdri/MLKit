fc2d9c20cd3fe572b035dd6f3146db2d3e625500
Admin <admin@Mendys-MacBook-Pro-13.local>
2023-11-07 08:51:21 +0200

    initial commit

---
+++ plinko/score.js
@@ -0,0 +1,37 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  const bucket = _.chain(outputs)
+	  .map(row => [distance(row[0]), row[3]])
+	  .sortBy(row => row[0])
+	  .slice(0, k)
+	  .countBy(row => row[1])
+	  .toPairs()
+	  .sortBy(row => row[1])
+	  .last()
+	  .first()
+	  .parseInt()
+    .value();
+
+  console.log('Your point will probably fall into', bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+} 
